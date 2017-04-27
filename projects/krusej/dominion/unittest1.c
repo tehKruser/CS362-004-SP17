b2cc8c8d33cca3b4681fb86ad97e7f3bcde7f739
Justin Kruse <krusej@flip1.engr.oregonstate.edu>
2017-04-26 19:01:48 -0700

    Working on unittest1.c

--- projects/krusej/dominion/unittest1.c
+++ projects/krusej/dominion/unittest1.c
@@ -21,21 +21,23 @@
 #include <stdlib.h>
 
 #define UNITTEST "unittest1"
-#define FUNCTEST "buyCard"
+#define FUNCTEST "buyCard()"
+
+// set NOISY_TEST to 0 to remove printfs from output
+#define NOISY_TEST 1
 
 int main() {
-    int i;
+    //int i;
     int seed = 1000;
     int numPlayer;
-    int maxBonus = 10;
     int p, r, cardNumber, cardCost, playerCoins, playerBuys, supplyPos, supplyCount;
     int k[10] = {adventurer, council_room, feast, gardens, mine, remodel, smithy, village, baron, great_hall};
 
     struct gameState G;
-    int maxHandCount = 5;
+    //int maxHandCount = 5;
 
-    printf("TESTING buyCard():\n");
-    for (numPlayer = 2; numplayer <= MAX_PLAYERS; numPlayer++) {
+    printf("----------------- %s Testing: %s ----------------\n", UNITTEST, FUNCTEST);
+    for (numPlayer = 2; numPlayer <= MAX_PLAYERS; numPlayer++) {
          for (p = 0; p < numPlayer; p++) {
             for(cardNumber = curse; cardNumber <= treasure_map; cardNumber++){
                 cardCost = getCost(cardNumber);
@@ -44,10 +46,10 @@ int main() {
                         for(supplyPos = 0; supplyPos <= treasure_map; supplyPos++){
                             for(supplyCount = 0; supplyCount <= 1; supplyCount++){
                                 memset(&G, 23, sizeof(struct gameState));   // clear the game state
-                                r = initializeGame(numPlayer, k, seed, &G); // initialize a new game
+                                initializeGame(numPlayer, k, seed, &G); // initialize a new game
                                 G.coins = playerCoins;
                                 G.supplyCount[supplyPos] = supplyCount;
-                                buyCard(supplyPos, &G);
+                                r = buyCard(supplyPos, &G);
                             }
                         }
                     }
