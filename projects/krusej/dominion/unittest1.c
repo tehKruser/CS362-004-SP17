7bd67262896b0e42e857fbd260f8715bc5fe7ed7
Justin Kruse <krusej@flip1.engr.oregonstate.edu>
2017-04-26 21:09:35 -0700

    Working on unittest1.c

--- projects/krusej/dominion/unittest1.c
+++ projects/krusej/dominion/unittest1.c
@@ -27,7 +27,7 @@
 #define NOISY_TEST 1
 
 int main() {
-    //int i;
+    int i;
     int seed = 1000;
     int numPlayer;
     int p, r, cardNumber, cardCost, playerCoins, playerBuys, supplyPos, supplyCount;
@@ -40,45 +40,45 @@ int main() {
     printf("----------------- %s Testing: %s ----------------\n", UNITTEST, FUNCTEST);
     for (numPlayer = 2; numPlayer <= MAX_PLAYERS; numPlayer++) {
          for (p = 0; p < numPlayer; p++) {
-            for(cardNumber = curse; cardNumber <= treasure_map; cardNumber++){
-                cardCost = getCost(cardNumber);
-                for(playerCoins = cardCost - 1; playerCoins <= cardCost; playerCoins++){
-                    for(playerBuys = 0; playerBuys <= 1; playerBuys++){
-                        for(supplyPos = 0; supplyPos <= treasure_map; supplyPos++){
-                            for(supplyCount = 0; supplyCount <= 1; supplyCount++){
-                                memset(&G, 23, sizeof(struct gameState));   // clear the game state
-                                initializeGame(numPlayer, k, seed, &G); // initialize a new game
-                                G.whoseTurn = p;
-                                G.coins = playerCoins;
-                                G.supplyCount[supplyPos] = supplyCount;
-                                G.numBuys = playerBuys;
+            for(playerBuys = 0; playerBuys <= 1; playerBuys++){
+                // iterate through normal cards
+                for(cardNumber = curse; cardNumber <= gold; cardNumber++){
+                    cardCost = getCost(cardNumber);
+                    for(supplyCount = 0; supplyCount <= 1; supplyCount++){
+                        for(playerCoins = cardCost - 1; playerCoins <= cardCost; playerCoins++){
 
-                                if(playerBuys == 0 || playerCoins < cardCost || supplyCount < 0) {
-                                    expectedCoins = G.coins;
-                                    expectedBuys = G.numBuys;
-                                    //expectedSupplyCount = G.supplyCount[supplyPos];
-                                    expectedPhase = G.phase;
-                                } else {
-                                    expectedCoins = G.coins - cardCost;
-                                    expectedBuys = G.numBuys - 1;
-                                    //expectedSupplyCount = G.supplyCount[supplyPos] - 1;
-                                    expectedPhase = 1;
-                                }
+                            memset(&G, 23, sizeof(struct gameState));   // clear the game state
+                            initializeGame(numPlayer, k, seed, &G); // initialize a new game
+                            G.whoseTurn = p;
+                            G.numBuys = playerBuys;
+                            G.supplyCount[cardNumber] = supplyCount;
+                            G.coins = playerCoins;
 
-                                printf("Test player %d with %d coin(s) and %d buy(s) in a %d player game.\n", p, playerCoins, playerBuys, numPlayer);
-
-                                r = buyCard(supplyPos, &G);
-#if (NOISY_TEST == 1)
-                                printf("G.coins = %d, expected = %d\n", G.coins, expectedCoins);
-                                printf("G.numBuys = %d, expected = %d\n", G.numBuys, expectedBuys);
-                                //printf("G.supplyCount[%d] = %d, expected = %d\n", supplyPos, G.supplyCount[supplyPos], expectedSupplyCount);
-                                printf("G.phase = %d, expected = %d\n", G.phase, expectedPhase);
-#endif
-                                assert(G.coins == expectedCoins);
-                                assert(G.numBuys == expectedBuys);
-                                //assert(G.supplyCount[supplyPos] == expectedSupplyCount);
-                                assert(G.phase == expectedPhase);
+                            if(playerBuys == 0 || playerCoins < cardCost || supplyCount == 0) {
+                                expectedCoins = G.coins;
+                                expectedBuys = G.numBuys;
+                                //expectedSupplyCount = G.supplyCount[supplyPos];
+                                expectedPhase = G.phase;
+                            } else {
+                                expectedCoins = G.coins - cardCost;
+                                expectedBuys = G.numBuys - 1;
+                                //expectedSupplyCount = G.supplyCount[supplyPos] - 1;
+                                expectedPhase = 1;
                             }
+
+                            printf("Test player %d with %d coin(s) and %d buy(s) in a %d player game, attempt to buy card %d for %d coins when there is %d left.\n", p, playerCoins, playerBuys, numPlayer, cardNumber, cardCost, supplyCount);
+
+                            r = buyCard(cardNumber, &G);
+#if (NOISY_TEST == 1)
+                            printf("G.coins = %d, expected = %d\n", G.coins, expectedCoins);
+                            printf("G.numBuys = %d, expected = %d\n", G.numBuys, expectedBuys);
+                            //printf("G.supplyCount[%d] = %d, expected = %d\n", supplyPos, G.supplyCount[supplyPos], expectedSupplyCount);
+                            printf("G.phase = %d, expected = %d\n", G.phase, expectedPhase);
+#endif
+                            assert(G.coins == expectedCoins);
+                            assert(G.numBuys == expectedBuys);
+                            //assert(G.supplyCount[supplyPos] == expectedSupplyCount);
+                            assert(G.phase == expectedPhase);
                         }
                     }
                 }
